9938c67f1dc356684e7cfa81d39c83366f074193
TestDp <Cristianmg13@hotmail.com>
2020-05-01 11:54:49 -0500

    actualizar migración

--- database/migrations/2019_01_03_162640_crear_tabla__jornada.php
+++ database/migrations/2019_01_03_162640_crear_tabla__jornada.php
@@ -16,35 +16,18 @@ class CrearTablaJornada extends Migration
         Schema::create('Tbl_Jornadas', function (Blueprint $table) {
             $table->increments('id');
             $table->date('Fecha');
-            $table->time('Inicio');     
-            $table->time('Fin');   
-             $table->integer('Descanso');  
+            $table->date('FechaFin');
+            $table->time('Inicio');
+            $table->time('Fin');
+            $table->integer('Descanso');
             $table->string('Lugar',100);
             $table->integer('Cupos');
-          // $table->integer('TipoCita');
-            //$table->boolean('EstadoReserva')->default(0);
-
-
-
-            //$table->integer('User_id')->unsigned()->nullable;
-           // $table->foreign('User_id')->references('id')->on('Tbl_Users');
-
             $table->integer('Tipo_Cita_id')->unsigned();
             $table->foreign('Tipo_Cita_id')->references('id')->on('Tbl_Tipos_Citas');
-
-            //$table->integer('Compania_id')->unsigned();
-            //$table->foreign('Compania_id')->references('id')->on('Tbl_Companias');
-
             $table->integer('Regional_id')->unsigned();
             $table->foreign('Regional_id')->references('id')->on('Tbl_Regionales');
-
-            //$table->integer('Sede_id')->unsigned()->nullable;
-            //$table->foreign('Sede_id')->references('id')->on('Tbl_Sedes');
-
-               $table->timestamps();
-
-             $table->unique(['Fecha', 'Tipo_Cita_id','Regional_id', 'Lugar', 'Inicio','Fin'],'jornada_indice_fk');
-      
+            $table->timestamps();
+            $table->unique(['Fecha', 'Tipo_Cita_id','Regional_id', 'Lugar', 'Inicio','Fin'],'jornada_indice_fk');
         });
     }
 

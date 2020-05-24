75fb5205b2d5a765bf127ea6aa9ccddd20e7f3da
DPSoluciones <juancamilo.blandon@gmail.com>
2020-05-24 17:35:17 -0500

    JCBU: arreglo guardado negocio cambio nombre sede

---
+++ app/Org_Saludables/Negocio/DTO/MEmpresa/CategoriaDTO.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: DPS-J
+ * Date: 23/05/2020
+ * Time: 4:20 PM
+ */
+
+namespace App\Org_Saludables\Negocio\DTO\MEmpresa;
+
+use App\Org_Saludables\Negocio\DTO\BaseDTO;
+
+class CategoriaDTO extends BaseDTO
+{
+    public $id;
+    public $Categoria;
+    public $Descripcion;
+}
